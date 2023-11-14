0bce807749499692dfc248fb202f4b115bf7b92d
alijm0806 <alijm1104@gmail.com>
2023-11-13 20:08:48 -0600

    Add toggle text color

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,11 @@
 import colorNames from 'colornames'
-const Input = ({ colorValue, setColorValue, setHexValue }) => {
+
+const Input = ({
+  colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText
+}) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
-      <label>Add Color Name</label>
+      <label>Add Color Name:</label>
       <input
         autoFocus
         type="text"
@@ -14,6 +17,12 @@ const Input = ({ colorValue, setColorValue, setHexValue }) => {
           setHexValue(colorNames(e.target.value));
         }}
       />
+      <button
+        type="button"
+        onClick={() => setIsDarkText(!isDarkText)}
+      >
+        Toggle Text Color
+      </button>
     </form>
   )
 }
--- src/Square.js
+++ src/Square.js
@@ -1,10 +1,11 @@
-import React from 'react'
-
-const Square = ({ colorValue, hexValue }) => {
+const Square = ({ colorValue, hexValue, isDarkText }) => {
   return (
     <section
       className="square"
-      style={{ backgroundColor: colorValue }}
+      style={{
+        backgroundColor: colorValue,
+        color: isDarkText ? "#000" : "#FFF"
+      }}
     >
       <p>{colorValue ? colorValue : "Empty Value"}</p>
       <p>{hexValue ? hexValue : null}</p>
@@ -13,7 +14,7 @@ const Square = ({ colorValue, hexValue }) => {
 }
 
 Square.defaultProps = {
-  colorValue: "empty color value"
+  colorValue: "Empty Color Value"
 }
 
 export default Square
